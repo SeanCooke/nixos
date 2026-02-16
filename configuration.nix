c45b238445be785fe9123d9042ab0ae8948bd7f1
Sean Cooke <SeanCooke8892@gmail.com>
2026-02-15 21:07:08 -0500

    Enabling GNOME dark mode.

--- configuration.nix
+++ configuration.nix
@@ -116,11 +116,20 @@
   #   enable = true;
   #   enableSSHSupport = true;
   # };
+  # Allowing GNOME configuration.
+  programs.dconf.enable = true;
 
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
+
+  # Enable GNOME Dark Mode.
+  services.gnome.gsettingsOverrides = {
+    "org.gnome.desktop.interface" = {
+      color-scheme = "prefer-dark";
+    };
+  }; 
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
