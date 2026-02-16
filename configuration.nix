3541881022d3efd59eb406bc5834b43cf38eb719
Sean Cooke <SeanCooke8892@gmail.com>
2026-02-15 21:32:53 -0500

    Enabling docker.

--- configuration.nix
+++ configuration.nix
@@ -110,7 +110,9 @@
   ];
 
   # Enabling docker.
-  virtualisation.docker = true;
+  virtualisation.docker = {
+    enable = true;
+  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
