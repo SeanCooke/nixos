c04254a59a138f8926d3ee40ba3e05a2336725e8
Sean Cooke <SeanCooke8892@gmail.com>
2026-08-22 14:42:51 -0400

    Managing Claude Code settings with dotfile in home manager.

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -39,6 +39,9 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
+    # Configuring Claude Code.
+    ".claude/settings.json".source = ./../apps/claude-code/settings.json;
+
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
